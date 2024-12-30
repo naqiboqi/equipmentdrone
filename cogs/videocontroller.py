b8e1b635b421798560d3d8c9846ebad2f1a61b5d
naqiboqi <naqihasn.2080@outlook.com>
2024-12-30 15:08:25 -0500

    Updated docstrings

--- cogs/videocontroller.py
+++ cogs/videocontroller.py
@@ -31,7 +31,7 @@ and detailed playback embeds.
     Custom exception class for handling connection errors.
 
 - **`InvalidVoiceChannel`**:
-    Exception for cases of invalid voice channels.
+    Custom exception class for handling invalid voice channel errors.
 
 - **`VideoController`**:
     Manages video playback in a Discord guild. Handles:
@@ -39,55 +39,19 @@ and detailed playback embeds.
     - Queue operations and embed updates.
     - Resource cleanup upon playback end.
 
-### Methods in `VideoController`:
-
-- **`connect_`**:
-    Connects the bot to the voice channel of the user who invoked the command.
-    - If the user is not in a voice channel, the command does nothing.
-
-- **`play_`**:
-    Adds the video(s) to the end of the queue given a direct link or name to search for. 
-    Handles playlists and private videos.
-
-- **`add_playlist_to_queue`**:
-    Adds all videos in the provided playlist URL to the queue.
-
-- **`add_video_to_queue`**:
-    Adds a single video to the end of the queue.
-
-- **`now_playing_`**:
-    Displays the current video being played along with its details in an embed format.
-
-- **`pause_`**:
-    Pauses or unpauses the currently playing video.
-
-- **`lyrics_`**:
-    Fetches and displays lyrics for the currently playing video or a provided song name.
-
-- **`get_queue_`**:
-    Displays the next up to ten videos in the queue.
-
-- **`remove_`**:
-    Removes a specific video from a given spot in the queue.
-
-- **`shuffle_`**:
-    Shuffles the queue, does not affect the currently playing video.
-
-- **`skip_`**:
-    Skips the currently playing video.
-
-- **`stop_`**:
-    Stops the currently playing video and cleans up the player.
-
-- **`change_volume`**:
-    Adjusts the player volume to the specified level (1-100).
+### Constants:
+- **`LYRICS_URL`**: The api url for obtaining video lyrics.
 
 ### Dependencies:
-- **`discord`**: For interacting with Discord APIs and sending embeds.
 - **`asyncio`**: For asynchronous event handling and queue management.
-- **`pytube`**: For downloading and processing YouTube videos.
+- **`discord`**: For interacting with Discord APIs and sending embeds.
 - **`itertools`**: For creating and managing queues.
+- **`pytube`**: For downloading and processing YouTube videos.
 - **`typing`**: For type hinting and function signatures.
+- **`discord.ext`**: For Discord bot command usage.
+- **`random.shuffle`**: For shuffling the queue.
+- **`videoplayer`**: For playing queued videos.
+- **`video`**: For representing video sources.
 """
 
 
@@ -118,15 +82,26 @@ class InvalidVoiceChannel(VoiceConnectionError):
 
 
 class VideoController(commands.Cog):
-    """Commands to represent media controls for the bot's video player."""
+    """Commands to represent media controls for the bot's video player.
+    
+    Params:
+        `bot` (commands.Bot): The bot instance.
+        `players` (dict[str, Player]): The video players associated with each server.
+        `player_ctx` (commands.Context): The most recently invoked context.
+    """
     def __init__(self, bot):
         self.bot = bot
         self.players = {}
-        self.player_ctx = None
+        self.player_ctx: commands.Context = None
 
-
-    async def cleanup(self, guild, ctx):
-        """Cleans up the server's player and the ffmpeg client."""
+    async def cleanup(self, guild: discord.Guild, ctx: commands.Context):
+        """Cleans up the server's player and the ffmpeg client.
+        
+        Params:
+        -------
+            `guild` (discord.Guild): The guild the command was invoked in.
+            `ctx` (commands.Context): The current `context` associated with a command.
+            """
         if ctx is None:
             ctx = self.player_ctx
 
@@ -144,9 +119,13 @@ class VideoController(commands.Cog):
         except KeyError as e:
             print(e)
 
-    def get_player(self, ctx):
-        """Retrieves the player for a guild, 
-        or creates it if the guild does not have one.
+    def get_player(self, ctx: commands.Context):
+        """Retrieves the player for a guild given a `context`, 
+        otherwise creates it if that guild has no player.
+        
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
         """
         try:
             player = self.players[ctx.guild.id]
@@ -160,20 +139,29 @@ class VideoController(commands.Cog):
         return player
 
     @commands.hybrid_command(name='join', aliases=['connect'])
-    async def connect_(self, ctx):
-        """Connects the bot to your current voice channel."""
+    async def connect_(self, ctx: commands.Context):
+        """Connects the bot to your current voice channel.
+        
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
+        """
         if ctx.author.voice:
             channel = ctx.message.author.voice.channel
-            await channel.connect()
+            return await channel.connect()
+
+        await ctx.send("You must be in a voice channel!")
 
     @commands.hybrid_command(name='play')
-    async def play_(self, ctx, *, video_search: str):
-        """Searches for a video and adds it to the queue. Can also add playlists given a link.
+    async def play_(self, ctx: commands.Context, *, video_search: str):
+        """Searches for a video and adds it to the queue.
         
-        Will also add all videos from a playlist in order,
-        if the given link is for a playlist.
+        Will also add all videos from a playlist in order, if the given link is for a playlist.
         
-        Any privated videos will not be added.
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
+            `video_search` (str): The title to search for, or a direct link to a video or playlist.
         """
         vc = ctx.voice_client
         if not vc:
@@ -181,8 +169,7 @@ class VideoController(commands.Cog):
 
         if not video_search:
             return await ctx.send(
-                "Please provide a video to look for.",
-                delete_after=10)
+                "Please provide a video to look for.", delete_after=10)
 
         player = self.get_player(ctx)
         self.player_ctx = ctx
@@ -191,28 +178,32 @@ class VideoController(commands.Cog):
         # than 3 seconds to respond to the command.
         await ctx.defer()
 
-        playlist_flag = "playlist?list=" in video_search
+        is_playlist = "playlist?list=" in video_search
         seek_time = int(video_search.split("&t=")[-1]) if "&t=" in video_search else 0
 
         try:
-            if playlist_flag:
-                await self.add_playlist_to_queue(ctx, player, video_search)
+            if is_playlist:
+                await self.add_playlist_to_queue_(ctx, player, video_search)
             else:
-                await self.add_video_to_queue(ctx, player, video_search, seek_time)
+                await self.add_video_to_queue_(ctx, player, video_search, seek_time)
 
         # A likely result if the playlist is privated.
         except Exception as e:
             await ctx.send(f"An error occurred: {e}", delete_after=10)
 
-    async def add_playlist_to_queue(
+    async def add_playlist_to_queue_(
         self, 
-        ctx, 
+        ctx: commands.Context, 
         player: videoplayer.Player, 
         playlist_url: str):
         """
         Adds all videos in the playlist at the given url to the queue.
         
-        Any privated videos will not be added.
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
+            `player` (Player): The `Player` for the guild that the command was invoked from.
+            `playlist_url` (str): The url for the `Playlist` to pull sources from.
         """
         playlist = pytube.Playlist(playlist_url)
         sources = await video.Video.get_sources(
@@ -225,14 +216,20 @@ class VideoController(commands.Cog):
             f"Added {len(playlist)} videos from **{playlist.title}** to the queue.",
             delete_after=10)
 
-    async def add_video_to_queue(
+    async def add_video_to_queue_(
         self, 
-        ctx, 
+        ctx: commands.Context, 
         player: videoplayer.Player, 
         video_search: str, 
         seek_time: int):
         """
         Adds a single video to the end of the queue.
+        
+        Params:
+            `ctx` (commands.Context): The current `context` associated with a command.
+            `player` (Player): The `Player` for the guild that the command was invoked from.
+            `video_search`: The title to search for.
+            `seek_time`: The time to start the video at.
         """
         source = await video.Video.get_source(
             ctx=ctx, search=video_search, 
@@ -244,8 +241,13 @@ class VideoController(commands.Cog):
         await ctx.send(f"Added {source.title} to the queue.", delete_after=10)
 
     @commands.hybrid_command(name='now', aliases=['np'])
-    async def now_playing_(self, ctx):
-        """Sends a embed showing info for the current vidoe."""
+    async def now_playing_(self, ctx: commands.Context):
+        """Sends a embed showing info for the current vidoe.
+        
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
+        """
         vc = ctx.voice_client
         player = self.get_player(ctx)
 
@@ -254,14 +256,18 @@ class VideoController(commands.Cog):
                 "I am not currently playing anything!",
                 delete_after=10)
 
-        if player.now_playing_embed is not None:
+        if player.now_playing_embed:
             await player.now_playing_embed.delete()
             now_playing_embed = await player.current.display()
             player.now_playing_embed = await ctx.send(embed=now_playing_embed)
 
     @commands.hybrid_command(name='pause')
-    async def pause_(self, ctx):
-        """Pauses or unpauses the current video."""
+    async def pause_(self, ctx: commands.Context):
+        """Pauses or unpauses the current video.
+        
+        Params:
+            `ctx` (commands.Context): The current `context` associated with a command.
+        """
         vc = ctx.voice_client
         player = self.get_player(ctx)
 
@@ -279,39 +285,69 @@ class VideoController(commands.Cog):
             await ctx.send("Paused", delete_after=10)
 
     @commands.hybrid_command(name='lyrics', aliases=['lyric'])
-    async def lyrics_(self, ctx, *, name: typing.Optional[str]):
-        """Gets the lyrics for the current video or search for a video given a name.
+    async def lyrics_(
+        self, 
+        ctx: commands.Context, 
+        *, 
+        video_search: typing.Optional[str]):
+        """
+        Gets the lyrics for the current video or search for a video given a name.
         
         Uses the text obtained through some-random-api's lyric function.
+        
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
+            `video_search` (str|None): The title whose lyrics to search for.
         """
         vc = ctx.voice_client
-        name = name or vc.source.title
+
+        if not vc or vc.is_connected():
+            return await ctx.send(
+                "I am not currently playing anything!", delete_after=10)
+
+        video_search = video_search or vc.source.title
 
         async with ctx.typing():
-            async with aiohttp.request("GET", LYRICS_URL + name, headers={}) as response:
-                if (not 200 <= response.status <= 299
-                or response.content_type != "application/json"):
-                    return await ctx.send("No lyrics found.")
+            try:
+                async with aiohttp.request(
+                    "GET", LYRICS_URL + video_search, headers={}) as response:
+                    # Ensure that the response to the GET request is valid.
+                    if (not 200 <= response.status <= 299
+                    or response.content_type != "application/json"):
+                        return await ctx.send("No lyrics found.")
 
-                data = await response.json()
-                try:
-                    if len(data['lyrics']) > 2000:
-                        await ctx.send(f"<{data['links']['genius']}>")
-                except KeyError:
-                    return await ctx.send("Couldn't find the lyrics for this video.")
+                    data = await response.json()
+
+                # Split the message to stay within the 2000 character limit.
+                lyrics = data["lyrics"]
+                if len(lyrics) > 2000:
+                    for chunk in [lyrics[i : i + 2000] for i in range(0, len(lyrics), 2000)]:
+                        await ctx.send(chunk)
+                        
+                    return
 
                 lyrics_embed = discord.Embed(
                     title=data["title"],
-                    description=data["lyrics"],
+                    description=lyrics,
                     color=0xa84300)
 
                 lyrics_embed.set_thumbnail(url=data['thumbnail']['genius'])
                 lyrics_embed.set_author(name=f"{data['author']}")
                 await ctx.send(embed=lyrics_embed)
 
+            except aiohttp.ClientError:
+                return await ctx.send("Failed to connect to the lyrics API.", delete_after=10)
+            except Exception as e:
+                return await ctx.send(f"An unknown error occured: {e}")
+
     @commands.hybrid_command(name='queue', aliases=['q', 'playlist'])
-    async def get_queue_(self, ctx):
-        """Shows the next 10 videos in the queue."""
+    async def get_queue_(self, ctx: commands.Context):
+        """Displays the next 10 videos in the queue within an embed.
+        
+        Params:
+            `ctx` (commands.Context): The current `context` associated with a command.
+        """
         vc = ctx.voice_client
         if not vc or not vc.is_connected():
             return await ctx.send(
@@ -324,8 +360,7 @@ class VideoController(commands.Cog):
         upcoming = list(itertools.islice(player.queue._queue, 0, 10))
         video_names = '\n'.join(
             f"{index + 1}. **{video['title']}** |"
-            f"`{datetime.timedelta(
-                seconds=video['duration'])}`"
+            f"`{datetime.timedelta(seconds=video['duration'])}`"
                 for index, video in enumerate(upcoming))
 
         queue_embed = discord.Embed(
@@ -336,8 +371,14 @@ class VideoController(commands.Cog):
         await ctx.send(embed=queue_embed)
 
     @commands.hybrid_command(name='removevideo', aliases=['r'])
-    async def remove_(self, ctx, *, spot: int):
-        """Removes a video at the given spot in the queue."""
+    async def remove_(self, ctx: commands.Context, *, spot: int):
+        """Removes a video at the given spot in the queue.
+        
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
+            `spot` (int): The index of the video to remove.
+        """
         player = self.get_player(ctx)
         videos_queue = player.queue._queue
         try:
@@ -345,24 +386,33 @@ class VideoController(commands.Cog):
             del videos_queue[spot - 1]
         except IndexError:
             return await ctx.send(
-                f"There is no video at spot {spot} in the queue.",
-                delete_after=10)
+                f"There is no video at spot {spot} in the queue.", delete_after=10)
 
         await ctx.send(
             f"Removed `{video_title}` from spot {spot} in the queue.", 
             delete_after=10)    
 
     @commands.hybrid_command(name='shuffle')
-    async def shuffle_(self, ctx):
-        """Shuffles all videos in the queue."""
+    async def shuffle_(self, ctx: commands.Context):
+        """Shuffles all videos in the queue.
+        
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
+        """
         player = self.get_player(ctx)
         videos = player.queue._queue
         shuffle(videos)
         await ctx.send("Shuffled.", delete_after=10)
 
     @commands.hybrid_command(name='skip')
-    async def skip_(self, ctx):
-        """Skips the currently playing video."""
+    async def skip_(self, ctx: commands.Context):
+        """Skips the currently playing video.
+        
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
+        """
         vc = ctx.voice_client
         if not vc or not vc.is_connected():
             return await ctx.send(
@@ -374,23 +424,34 @@ class VideoController(commands.Cog):
         await ctx.send("Skipped the video.", delete_after=10)
 
     @commands.hybrid_command(name='stop')
-    async def stop_(self, ctx):
-        """Stops the currently playing video."""
+    async def stop_(self, ctx: commands.Context):
+        """Stops the currently playing video.
+        
+        Params:
+        -------
+            `ctx` (commands.Context): The current `context` associated with a command.
+        """
         vc = ctx.voice_client
         if not vc or not vc.is_connected():
-            return await ctx.send("I am not currently playing anything!", delete_after=10)
+            return await ctx.send(
+                "I am not currently playing anything!", delete_after=10)
 
         await self.cleanup(ctx.guild, ctx)
         await ctx.send("Stopped the video.", delete_after=10)
 
     @commands.hybrid_command(name='volume', aliases=['vol'])
-    async def change_volume(self, ctx, *, vol: int):
-        """Sets the player volume, must be between `1` and `100`."""
+    async def change_volume_(self, ctx: commands.Context, *, vol: int):
+        """Sets the player volume to the given value.
+        
+        Params:
+            `ctx` (commands.Context): The current `context` associated with a command.
+            `vol` (int): The new volume level, must be between `1` and `100`.
+        """
         vc = ctx.voice_client
         if not vc or not vc.is_connected():
             return await ctx.send(
                 "I am not currently connected to voice!", delete_after=10)
-        elif ((not 1 <= vol <= 100) or (vol is None)):
+        elif (not 1 <= vol <= 100) or (vol is None):
             return await ctx.send(
                 "Please enter a value between 1 and 100.", delete_after=10)
         elif vc.source:
@@ -401,5 +462,5 @@ class VideoController(commands.Cog):
         await ctx.send(f"Set the volume to {vol}%.", delete_after=10)
 
 
-async def setup(bot):
+async def setup(bot: commands.Bot):
     await bot.add_cog(VideoController(bot))
