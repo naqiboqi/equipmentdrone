65f4306b1965a74f1725f1cbffdcbfbd05d5e582
naqih <na828@drexel.edu>
2025-01-06 02:15:59 -0500

    Renamed directory event_log to utils

--- cogs/event_log/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-The `log` package. Includes views for interactive embeds containing log events.
-"""
-
-
-from .eventlog import EventLog
-from .logview import LogView
---
+++ cogs/utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+The `utils` package. Includes interactive embeds containing log events and loading files
+"""
+
+
+from .eventlog import EventLog
+from .logview import LogView
