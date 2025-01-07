ce91b49ed01a9446947efc6470bc448027d89856
naqiboqi <naqihasn.2080@outlook.com>
2025-01-07 01:47:29 -0500

    Updated docstrings

--- cogs/video_load/video.py
+++ cogs/video_load/video.py
@@ -45,6 +45,7 @@ Key Features:
 """
 
 
+
 import asyncio
 import datetime
 import discord
@@ -57,32 +58,36 @@ from yt_dlp import YoutubeDL
 from .progress import ProgressBar
 
 
-
 YTDL_FORMATS = {
-    'format' : 'bestaudio/best',
-    'outtmpl' : 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames' : True,
-    'noplaylist' : True,
-    'nocheckcertificate' : True,
-    'ignoreerrors' : False,
-    'logtostderr' : False,
-    'quiet' : True,
+    'format': 'bestaudio/best',
+    'outtmpl': 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
     'no_warnings': True,
-    'default_search' : 'auto',
-    'source_address' : '0.0.0.0'
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
 }
 
 
 ytdl = YoutubeDL(YTDL_FORMATS)
 
 
-
-async def get_ffmpeg_options(seek_time=0.00):
+async def get_ffmpeg_options(seek_time: float = 0.00) -> dict:
     """Returns the applicable ffmpeg options for the requested video.
-    
+
     Params:
     -------
-    `seek_time` (float): Time to seek to in the video, in seconds.
+    seek_time: float
+        Time to seek to in the video, in seconds.
+
+    Returns:
+    --------
+    dict
+        The ffmpeg options for the video.
     """
     return {
         'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
@@ -92,68 +97,84 @@ async def get_ffmpeg_options(seek_time=0.00):
 
 class Video(discord.PCMVolumeTransformer):
     """Represents a Youtube video, with applicable attributes to store the video metadata.
-    
+
     Inherits from `PCMVolumeTransformer` to allow volume control of the source.
 
     Attributes:
     -----------
-        `source` (FFmpegPCMAudio): The audio source obtained through ffmpeg.
-        `data` (dict[str, str|int]): The metadata for the audio source.
-        `title` (str): The title of the source video.
-        `web_url` (str): The web url that the source video originated from.
-        `video_id` (str): The video-id of the source video.
-        `thumbnail` (str): The thumbnail url of the source video.
-        `elapsed_time` (float): The elapsed time of the video, in seconds.
-        `progress` (ProgressBar): Represents a video's progress bar with a slider denoting the elapsed time.
-        `seeked_time` (float): The start/seek time of the video in seconds, defaults to 0.00.
-        `start_time` (float): The start time of the video.
+    source: discord.FFmpegPCMAudio
+        The audio source obtained through ffmpeg.
+    data: dict[str, str | int]
+        The metadata for the audio source.
+    title: str
+        The title of the source video.
+    web_url: str
+        The web URL that the source video originated from.
+    video_id: str
+        The video-id of the source video.
+    thumbnail: str
+        The thumbnail URL of the source video.
+    elapsed_time: float
+        The elapsed time of the video, in seconds.
+    seeked_time: float
+        The start/seek time of the video in seconds, defaults to 0.00.
+    start_time: float
+        The start time of the video.
+    progress: ProgressBar
+        Represents a video's progress bar with a slider denoting the elapsed time.
     """
     def __init__(
         self,
-        source: discord.player.FFmpegPCMAudio,
-        *, 
-        data: dict[str, str|int], 
-        requester: discord.member.Member, 
-        duration: int):
-
+        source: discord.FFmpegPCMAudio,
+        *,
+        data: dict[str, str | int],
+        requester: discord.member.Member,
+        duration: int
+    ) -> None:
         super().__init__(source)
-        self.duration = duration
-        self.requester = requester
-        self.title = data.get('title')
-        self.web_url = data.get('webpage_url')
-        self.video_id = self.web_url.split("=", 1)[1]
-        self.thumbnail = F"https://i1.ytimg.com/vi/{self.video_id}/hqdefault.jpg"
+        self.duration: int = duration
+        self.requester: discord.member.Member = requester
+        self.title: str = data.get('title')
+        self.web_url: str = data.get('webpage_url')
+        self.video_id: str = self.web_url.split("=", 1)[1]
+        self.thumbnail: str = F"https://i1.ytimg.com/vi/{self.video_id}/hqdefault.jpg"
 
         self.elapsed_time = 0.00
-        self.progress: ProgressBar = None
         self.seeked_time = 0.00
         self.start_time = 0.00
-
+        self.progress: ProgressBar = None
 
     def __getitem__(self, item: str):
-        """Allows access to attributes similar to a dict."""
+        """Allows access to attributes similar to a dict.
+        
+        Params:
+        -------
+        item: str
+            The attribute name to access.
+        """
         return self.__getattribute__(item)
 
     @classmethod
     async def get_sources(
-        cls, 
-        ctx, 
-        playlist: Playlist, 
-        *, 
+        cls,
+        ctx: discord.ext.commands.Context,
+        playlist: Playlist,
+        *,
         loop: asyncio.AbstractEventLoop,
-        download=False):
-        """
-        Gets the source object for each video in the playlist through its url.
-        
-        Params:
-            `ctx` (Context): The current context associated with a command.
-            `playlist` (pytube.Playlist): Contains the videos to obtain the sources from.
-            `loop` (AbstractEventLoop): The event loop to use for asynchronous operations. 
-                If not provided, the default event loop for the current thread will be used.
-                
-            `download` (bool): Whether to download the video or not.
+        download: bool = False):
+        """Gets the source object for each video in the playlist through its URL.
 
-        Note that any privated videos will be skipped.
+        Params:
+        -------
+        ctx: discord.ext.commands.Context
+            The current context associated with a command.
+        playlist: Playlist
+            Contains the videos to obtain the sources from.
+        loop: asyncio.AbstractEventLoop
+            The event loop to use for asynchronous operations. If not provided, 
+            the default event loop for the current thread will be used.
+        download: bool
+            Whether to download the video or not.
         """
         tasks = [cls.get_source(ctx=ctx, search=url, loop=loop, download=download)
             for url in playlist.video_urls]
@@ -162,25 +183,29 @@ class Video(discord.PCMVolumeTransformer):
 
     @classmethod
     async def get_source(
-        cls, 
-        ctx, 
-        search: str, 
-        *, 
-        loop: asyncio.AbstractEventLoop, 
-        download=False, 
-        seek_time=0.00):
-        """
-        Gets the source for the video obtained from searching for the `string`, returning the first
-        result from Youtube.
-        
-        Params:
-            `ctx` (Context): The current context associated with a command.
-            `playlist` (pytube.Playlist): Contains the videos to obtain the sources from.
-            `loop` (AbstractEventLoop): The event loop to use for asynchronous operations.
-                If not provided, the default event loop for the current thread will be used.
+        cls,
+        ctx: discord.ext.commands.Context,
+        search: str,
+        *,
+        loop: asyncio.AbstractEventLoop,
+        download: bool = False,
+        seek_time: float = 0.00):
+        """Gets the source for the video obtained from searching for the `string`,
+        returning the first result from YouTube.
 
-            `download` (bool): Whether to download the video or not.
-            `seek_time` (float): The seek time of the video in seconds.
+        Params:
+        -------
+        ctx: discord.ext.commands.Context
+            The current context associated with a command.
+        search: str
+            The string to search for.
+        loop: asyncio.AbstractEventLoop
+            The event loop to use for asynchronous operations. If not provided, 
+            the default event loop for the current thread will be used.
+        download: bool
+            Whether to download the video or not.
+        seek_time: float
+            The seek time of the video in seconds.
         """
         loop = loop or asyncio.get_event_loop()
 
@@ -205,18 +230,19 @@ class Video(discord.PCMVolumeTransformer):
 
     @classmethod
     async def prepare_stream(
-        cls, 
-        data: "Video", 
-        *, 
+        cls,
+        data: "Video",
+        *,
         loop: asyncio.AbstractEventLoop):
-        """
-        Prepares a video stream for playing, and returns an instance of that video object.
+        """Prepares a video stream for playing, and returns it.
 
         Params:
         -------
-            `data` (Video): The video data object to prepare for streaming.
-            `loop` (AbstractEventLoop): The event loop to use for asynchronous operations.
-                If not provided, the default event loop for the current thread will be used.
+        data: Video
+            The video data object to prepare for streaming.
+        loop: asyncio.AbstractEventLoop
+            The event loop to use for asynchronous operations. If not provided, 
+            the default event loop for the current thread will be used.
         """
         loop = loop or asyncio.get_event_loop()
         requester = data['requester']
@@ -226,7 +252,7 @@ class Video(discord.PCMVolumeTransformer):
         data = await loop.run_in_executor(None, to_run)
 
         return cls(
-            discord.FFmpegPCMAudio(data['url']), data=data, 
+            discord.FFmpegPCMAudio(data['url']), data=data,
             requester=requester, duration=duration)
 
     def get_duration_datetime(self):
@@ -235,22 +261,25 @@ class Video(discord.PCMVolumeTransformer):
 
     async def start(self, start_time: float, volume: float):
         """Initializes the settings for the video source object.
-        
+
         Params:
         -------
-            `start_time` (float): The start time of the video.
-            `volume` (float): The current volume of the player as a percentage.
+        start_time: float
+            The start time of the video.
+        volume: float
+            The current volume of the player as a percentage.
         """
         self.progress = ProgressBar(self.duration)
         self.start_time = start_time
         self.volume = volume
 
-    async def display(self, elapsed_time=0.00):
-        """Returns an embed containing the details of video source object.
+    async def display(self, elapsed_time: float = 0.00):
+        """Returns an embed containing the details of the video source object.
 
         Params:
         -------
-            `elapsed_time` (float): The elapsed time of the video, in seconds.
+        elapsed_time: float
+            The elapsed time of the video, in seconds.
         """
         self.elapsed_time = elapsed_time
         elapsed_field = time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
