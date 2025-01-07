fed01507cdeb13942dd4ef603223e078ec8e00f6
naqiboqi <naqihasn.2080@outlook.com>
2025-01-07 00:16:09 -0500

    Updated docstring

--- cogs/battleship_game/ship.py
+++ cogs/battleship_game/ship.py
@@ -1,5 +1,5 @@
 """
-This module defines the `Ship` class, representing a ship
+This module defines the ship class, representing a ship
 in a player's fleet in the `Battleship` game.
 """
 
@@ -9,26 +9,33 @@ class Ship():
     
     Attributes:
     ----------
-        `size` (int): The size and starting health of the `ship`
-        `name` (str): The name of the `ship`
-        `ship_class` (str): The class (or type) of `ship`
+        size : int
+            The size and starting health of the ship.
+        name : str
+            The name of the ship.
+        ship_class : str
+            The class (or type) of ship.
         
-        `health` (list[bool]): Stores the health of the ship's components
+        health : list[bool] 
+            Stores the health of the ship's components.
         
-        For example, a `Ship` of size `4` could look like:
+        For example, a ship of size `4` could look like:
         
         `self.health = [True, True, True, True]`
         
-        A `Ship` with a health array of all `False` means that it has sunk
+        A ship with a health array of all `False` means that it has sunk.
         
-        `locs` (list[tuple[int, int]]): Stores the `(y, x)` locations of the ship's components
+        locs : list[tuple[int, int]] 
+            Stores the `(y, x)` locations of the ship's components.
         
-        For example, a `Ship` of size `4` could look like:
+        For example, a ship of size `4` could look like:
 
         `self.locs = [(1, 0), (2, 0), (3, 0), (4, 0)]`
         
-        `final_placed` (bool): Whether or not the `ship`'s position is finalized
-        `placed_before` (bool): Whether or not the `ship` has been placed before
+        final_placed : bool
+            Whether or not the ship's position is finalized.
+        placed_before : bool
+            Whether or not the ship has been placed before.
     """
     def __init__(self, size: int):
         self.size = size
@@ -42,25 +49,28 @@ class Ship():
     def take_damage_at(self, y: int, x: int):
         """Damages the targeted section of the ship.
         
-        Sets the associated `Ship` section's health at that `(y, x)` coordinate of that board to False by
+        Sets the associated ship section's health at that `(y, x)` coordinate of that board to False by
         getting the index of that coordinate in `self.locs`.
 
         Params:
         -------
-            `y` (int): The y coordinate of the location.
-            `x` (int): The x coordinate of the location.
+            y : int
+                The y coordinate of the location.
+            x : int
+                The x coordinate of the location.
         """
         section = self.locs.index((y, x))
         self.health[section] = False
 
     def is_sunk(self):
-        """Returns whether the `Ship` has lost all of its health."""
+        """Returns whether the ship has lost all of its health."""
         return all(hp is False for hp in self.health)
 
     def __str__(self):
-        """Returns a string representation of the `ship`.
+        """Returns a string representation of the ship.
         
-        For example, `The SS Varmland, a Destroyer. Currently at 2 health.`"""
+        For example:
+            `The SS Varmland, a Destroyer. Currently at 2 health.`"""
         return f"""
             The **{self.name}**, a {self.ship_class} (size: {self.size})
             Currently at {self.health.count(True)} health."""
