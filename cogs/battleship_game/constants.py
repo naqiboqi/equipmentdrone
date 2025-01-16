49c4fda52fefd4bd9ff1a327ef82b33583a46f3b
naqiboqi <naqihasn.2080@outlook.com>
2025-01-16 16:01:50 -0500

    Added using custom emojis

--- cogs/battleship_game/constants.py
+++ cogs/battleship_game/constants.py
@@ -11,13 +11,17 @@ from ..utils import JsonLoader
 SHIP_NAMES_FILE = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "../../data/ship_names.json"))
 
+ship_names = JsonLoader.load_json(SHIP_NAMES_FILE)
+"""Stores the possible ship names for each country and class of ship."""
 
 BOT_MESSAGES_FILE = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "../../data/bot_messages.json"))
 
-
-ship_names = JsonLoader.load_json(SHIP_NAMES_FILE)
-"""Stores the possible ship names for each country and class of ship."""
-
 bot_messages = JsonLoader.load_json(BOT_MESSAGES_FILE)
 """Stores the possible bot messages for attacking and defending."""
+
+EMOJIS_FILE = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../data/emojis.json"))
+
+emojis = JsonLoader.load_json(EMOJIS_FILE)
+"""Stores the emojis."""
