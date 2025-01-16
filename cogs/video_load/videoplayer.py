87ecb7e0aa681d2fb03783e1dbb7d2dc6ed897c2
naqiboqi <naqihasn.2080@outlook.com>
2025-01-16 15:46:01 -0500

    Added custom emojis

--- cogs/video_load/videoplayer.py
+++ cogs/video_load/videoplayer.py
@@ -1,18 +1,18 @@
 """
 This module implements an audio player for a Discord bot, designed to handle media 
 playback and user interaction within a server (guild). It utilizes the `discord.py` 
-library for bot functionality and provides features like queue management, 
+library for bot functionality and provides features like playlist management, 
 progress tracking, and detailed playback embeds.
 
 Key Features:
 - **Audio Playback**:
-    - Manage playback of queued audio sources.
+    - Manage playback of playlistd audio sources.
     - Support for pausing, resuming, and looping.
     - Adjust playback volume dynamically.
 
-- **Queue Management**:
-    - Handle a queue of `Video` objects for seamless playback.
-    - Add videos to the front of the queue or in order of requests.
+- **playlist Management**:
+    - Handle a playlist of `Video` objects for seamless playback.
+    - Add videos to the front of the playlist or in order of requests.
 
 - **Embed Generation**:
     - Create real-time playback embeds displaying:
@@ -22,18 +22,18 @@ Key Features:
     - Update existing embeds as playback progresses.
 
 - **Asynchronous Operations**:
-    - Use asyncio for efficient non-blocking queue and playback handling.
+    - Use asyncio for efficient non-blocking playlist and playback handling.
     - Manage playback state and time tracking.
 
 ### Classes:
 - **`Player`**:
     Represents the media player for a Discord server. Manages:
     - Playback control, including start, pause, resume, and loop.
-    - Queue operations and embed updates.
+    - playlist operations and embed updates.
     - Cleanup and resource management when playback ends.
 
 ### Dependencies:
-- **`asyncio`**: For asynchronous event handling and queue management.
+- **`asyncio`**: For asynchronous event handling and playlist management.
 - **`discord`**: For interacting with Discord APIs and sending embeds.
 - **`time`**: For tracking playback and elapsed time.
 - **`video`**: Represents the media source for playback.
@@ -47,15 +47,20 @@ import pytube
 import time
 
 from discord.ext import commands
+from .constants import emojis
 from .video import Video
 from .videoplaylist import VideoPlaylist
 from .videoplayerview import VideoPlayerView
 
 
 
+playlist_emoji = emojis.get("playlist")
+
+
+
 class VideoPlayer:
     """Represents a video player that awaits recieving `Video` objects
-    from a queue and playing them as needed.
+    from a playlist and playing them as needed.
     
     Is created when the `bot` joins a voice channel, and is destroyed when
     the `bot` leaves the voice channel. Each `Guild` will have its own `Player`.
@@ -73,14 +78,14 @@ class VideoPlayer:
         guild : discord.Guild
             The current guild associated with a command.
         next : asyncio.Event
-            The signal to start the next video in the queue.
-        queue : asyncio.Queue
-            The queue storing upcoming videos.
+            The signal to start the next video in the playlist.
+        playlist : asyncio.playlist
+            The playlist storing upcoming videos.
         current : Video
             The currently playing video.
         now_playing_embed : discord.Message
             The embed storing the player's current information.
-        queue_embed : discord.Message
+        playlist_embed : discord.Message
             The embed storing the upcoming videos' information.
         loop : bool
             Whether the player is looping the current video.
@@ -110,11 +115,12 @@ class VideoPlayer:
         await self.bot.wait_until_ready()
 
         while not self.bot.is_closed():
-            print("are we waiting?")
             await self.video_playlist.ready.wait()
 
             self.next.clear()
             source = self.video_playlist.now_playing.content
+            print(source)
+            
             if not isinstance(source, Video):
                 print(f"Error: The next video is not valid: {source}")
                 continue
@@ -122,64 +128,33 @@ class VideoPlayer:
             self.current = source
             start_time = time.perf_counter() - source.seeked_time
             source.start(start_time, self.volume)
-            
-            self.guild.voice_client.play(source, after=lambda e: self.bot.loop.call_soon_threadsafe(self.video_playlist.advance))
+
+            if self.current is not None and isinstance(self.current, discord.AudioSource):
+                print(f"playing {self.current.title}")
+                self.guild.voice_client.play(self.current, after=self.handle_after_play)
+            else:
+                print(f"Error: Invalid audio source {self.current}")
 
             await self.show_player_details()
             await self.timer(self.current.start_time)
+            print("timer done!")
             
-            source.cleanup()
+            if not self.video_playlist.loop_one:
+                source.cleanup()
+                
             self.current = None
 
-    async def add_videos_to_playlist(
-        self, 
-        ctx: commands.Context, 
-        playlist_url: str):
-        """
-        Adds all videos in the playlist at the given url to the queue.
+    def handle_after_play(self, error):
+        if error:
+            print(f"Error playing video {error}")
+            
+        if self.video_playlist.loop_one:
+            print("replaying")
+            self.guild.voice_client.play(self.current)
+        else:
+            print("advancing")
+            self.video_playlist.advance()
         
-        Params:
-        -------
-            ctx : commands.Context
-                The current context associated with a command.
-            playlist_url : str
-                The url for the `Playlist` to pull sources from.
-        """
-        playlist = pytube.Playlist(playlist_url)
-        sources = await Video.get_sources(
-            ctx=ctx, playlist=playlist, loop=self.bot.loop, download=False)
-
-        for source in sources:
-            await self.video_playlist.add_to_end(source)
-
-        await ctx.send(
-            f"Added {len(playlist)} videos from **{playlist.title}** to the queue.",
-            delete_after=10)
-
-    async def add_video_to_playlist(
-        self, 
-        ctx: commands.Context,
-        video_search: str, 
-        seek_time: int):
-        """
-        Adds a single video to the end of the queue.
-        
-        Params:
-            ctx : commands.Context
-                The current context associated with a command.
-            video_search : str
-                The title to search for.
-            seek_time : float
-                The time to start the video at.
-        """
-        source = await Video.get_source(
-            ctx=ctx, search=video_search, 
-            loop=self.bot.loop, 
-            download=False, 
-            seek_time=seek_time)
-
-        await self.video_playlist.add_to_end(source)
-        await ctx.send(f"Added {source.title} to the queue.", delete_after=10)
 
     async def timer(self, start_time: float):
         """Keeps track of the video's runtime, and calls update_player_details()
@@ -198,14 +173,13 @@ class VideoPlayer:
             if self.guild.voice_client.is_playing():
                 elapsed_time = time.perf_counter() - (start_time + paused_time)
                 await self.show_player_details(elapsed_time)
-
             elif self.paused:
                 paused_time += 1.00
             else:
                 # Case for when the video is finished playing
                 elapsed_time = time.perf_counter() - (start_time + paused_time)
                 await self.show_player_details(elapsed_time)
-                break    
+                break
 
     async def show_player_details(self, elapsed_time: float=0.00):
         """Creates and sends an embed showing the current details of the player:
@@ -230,6 +204,63 @@ class VideoPlayer:
             view = VideoPlayerView(self.bot, self.ctx)
             self.now_playing_message = await self.channel.send(embed=now_playing_embed, view=view)
             print(f"Error {e}")
+
+    async def add_videos_to_playlist(
+        self, 
+        ctx: commands.Context, 
+        playlist_url: str):
+        """
+        Adds all videos in the playlist at the given url to the playlist.
+        
+        Params:
+        -------
+            ctx : commands.Context
+                The current context associated with a command.
+            playlist_url : str
+                The url for the `Playlist` to pull sources from.
+        """
+        playlist = pytube.Playlist(playlist_url)
+        sources = await Video.get_sources(
+            ctx=ctx, playlist=playlist, loop=self.bot.loop, download=False)
+
+        for source in sources:
+            await self.video_playlist.add_to_end(source)
+
+        await ctx.send(
+            f"Added {len(playlist)} videos from **{playlist.title}** to the playlist {playlist_emoji}",
+            delete_after=10)
+
+    async def add_video_to_playlist(
+        self, 
+        ctx: commands.Context,
+        video_search: str, 
+        seek_time: int):
+        """
+        Adds a single video to the end of the playlist.
+        
+        Params:
+            ctx : commands.Context
+                The current context associated with a command.
+            video_search : str
+                The title to search for.
+            seek_time : float
+                The time to start the video at.
+        """
+        source = await Video.get_source(
+            ctx=ctx, search=video_search, 
+            loop=self.bot.loop, 
+            download=False, 
+            seek_time=seek_time)
+
+        await self.video_playlist.add_to_end(source)
+        await ctx.send(f"Added {source.title} to the playlist {playlist_emoji}", delete_after=10)
+            
+    async def cleanup(self):
+        try:
+            await self.now_playing_message.delete()
+            await self.playlist_message.delete()
+        except AttributeError as e:
+            print(e)
 
     def destroy(self, guild: discord.Guild):
         """Disconnects and cleans the player.
