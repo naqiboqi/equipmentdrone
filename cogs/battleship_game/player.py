275cde9b3f72272ed6d9955d33eda6438e573501
naqiboqi <naqihasn.2080@outlook.com>
2025-01-27 19:06:44 -0500

    Added properties for player name and if they are defeated

--- cogs/battleship_game/player.py
+++ cogs/battleship_game/player.py
@@ -82,7 +82,7 @@ class Player():
         self.attack_board = AttackBoard()
         self.fleet = [Ship(size) for size in [2, 3, 3, 4, 5]]
         self.member = member
-    
+
         self.country: str = None
         self.placement_message: discord.Message = None
         self.defense_board_message: discord.Message = None
@@ -90,7 +90,22 @@ class Player():
 
     def __eq__(self, other: "Player"):
         return self.member == other.member
-    
+
+    @property
+    def mention(self):
+        """Mention the player."""
+        return self.member.mention
+
+    @property
+    def name(self):
+        """The player's Discord name."""
+        return self.member.name
+
+    @property
+    def is_defeated(self):
+        """If the player has no remaining ships."""
+        return all(ship.is_sunk() for ship in self.fleet)
+
     def set_ship_names(self, names: dict[str, list[str]]):
         """Sets the names of the ships in the player's fleet.
         
@@ -105,7 +120,7 @@ class Player():
             ship = self.fleet[i]
             ship.ship_class = ship_class
             ship.name = random.choice(names[ship_class])
-    
+
     async def choose_ship_placement(self, ctx: commands.Context):
         """Handles the player's ship placement by using buttons and a dropdown for ship selection.
         
@@ -116,14 +131,13 @@ class Player():
         """
         embed = self.defense_board.get_ship_placement_embed()
         view = BoardView(self.defense_board, self.fleet)
-        
+
         try:
             self.placement_message = await self.member.send(embed=embed, view=view)
         except discord.errors.Forbidden:
             await ctx.send(f"Could not send the game board to {self.member.mention}, "
                 f"please check you DM settings.")
-        
-        # Don't exit until all ships are placed
+
         while not (all(ship.confirmed for ship in self.fleet)):
             await sleep(5)
 
@@ -139,19 +153,19 @@ class Player():
             If the placing player is a bot or not."""
         self.defense_board.random_place_ships(self.fleet, bot_player)
 
-    async def update_board_states(self):
-        """Updates the boards in the player's direct messages."""
-        fleet_embed = self.defense_board.get_embed()
-        tracking_embed = self.attack_board.get_embed()
-        self.defense_board_message = await self.defense_board_message.edit(embed=fleet_embed)
-        self.attack_board_message = await self.attack_board_message.edit(embed=tracking_embed)
-        
     async def send_board_states(self):
         """Sends the player's boards as a direct message."""
-        fleet_embed = self.defense_board.get_embed()
-        tracking_embed = self.attack_board.get_embed()
+        fleet_embed = self.defense_board.embed
+        tracking_embed = self.attack_board.embed
         self.defense_board_message = await self.member.send(embed=fleet_embed)
         self.attack_board_message = await self.member.send(embed=tracking_embed)
+
+    async def update_board_states(self):
+        """Updates the boards in the player's direct messages."""
+        fleet_embed = self.defense_board.embed
+        tracking_embed = self.attack_board.embed
+        self.defense_board_message = await self.defense_board_message.edit(embed=fleet_embed)
+        self.attack_board_message = await self.attack_board_message.edit(embed=tracking_embed)
 
     def get_ship_at(self, y: int, x: int):
         """Returns the player's ship that is at the given `(y, x)` location.
@@ -168,6 +182,4 @@ class Player():
             if loc in ship.locs:
                 return ship
 
-    def is_defeated(self):
-        """Returns whether all of the player's ships are sunk."""
-        return all(ship.is_sunk() for ship in self.fleet)
+        return None
