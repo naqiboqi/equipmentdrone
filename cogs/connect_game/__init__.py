67825cb304625670c0241372e7ecef3ec28f9f89
naqiboqi <naqihasn.2080@outlook.com>
2025-01-30 03:55:37 -0500

    Updated filenames to be more clear

--- cogs/connect_game/__init__.py
+++ cogs/connect_game/__init__.py
@@ -5,7 +5,7 @@ The `Connect Four` game package. Includes game elements and constants.
 
 
 from .botlogic import BotLogic
-from .connectfourboard import Board
-from .connectfourplayer import ConnectFourPlayer
+from .connect_board import Board
+from .connect_player import ConnectFourPlayer
 from .game import Game
 from .gameutils import is_winner
